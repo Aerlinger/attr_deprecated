2b30a17352f2dd83272fb8f9663eaf78d9b4cf1f
Anthony Erlinger <aerlinger@gmail.com>
2014-04-26 22:30:43 -0400

    Define and test attr_deprecated class method on AttrDeprecated::Model module

--- lib/attr_deprecated.rb
+++ lib/attr_deprecated.rb
@@ -1,5 +1,22 @@
 require "attr_deprecated/version"
 
+require 'active_support'
+
 module AttrDeprecated
-  # Your code goes here...
+  module Model
+
+    def self.included(base)
+      base.send :extend, ClassMethods
+    end
+
+    module ClassMethods
+      def attr_deprecated(*attr_names)
+        # TODO
+      end
+    end
+  end
+end
+
+ActiveSupport.on_load(:active_record) do
+  include AttrDeprecated::Model
 end
--- spec/attr_deprecated_spec.rb
+++ spec/attr_deprecated_spec.rb
@@ -4,4 +4,12 @@ describe "Sample spec" do
   specify "AttrDeprecated is defined" do
     defined?(AttrDeprecated).should be_true
   end
+
+  specify "A class that extends AttrDeprecated::Model will have attr_deprecated defined" do
+    Foo = Class.new
+
+    Foo.class_eval { include AttrDeprecated::Model }
+
+    Foo.methods.should include(:attr_deprecated)
+  end
 end

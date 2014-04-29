0f8f587a21a9534548f5ba8dcace4ae966f2e879
Anthony Erlinger <aerlinger@gmail.com>
2014-04-28 23:59:19 -0400

    Update module scope for DeprecatedAttributeLogger

--- lib/notifiers/deprecation_logger.rb
+++ lib/notifiers/deprecation_logger.rb
@@ -1,29 +1,35 @@
-class DeprecatedAttributeLogger
-  def initialize(target)
-    @target = target
-  end
-
-  def logger
-    @target.logger
-  end
-
-  def logger?
-    @target.respond_to?(:logger) && @target.logger
-  end
-
-  def backtrace
-    if defined? Rails
-      Rails.backtrace_cleaner.clean(caller)
-    else
-      caller
+module AttrDeprecated
+  class DeprecatedAttributeLogger
+    def initialize(target)
+      @target = target
     end
-  end
 
-  def log_deprecated_attribute_usage(klass, attrs)
-    if logger?
-      logger.warn do
-        "WARNING: Called deprecated attribute for #{klass.name}: #{attrs.join(', ')}\n" +
-          backtrace.map { |trace| "\t#{trace}" }.join("\n")
+    def logger
+      @target.logger
+    end
+
+    def logger?
+      @target.respond_to?(:logger) && @target.logger
+    end
+
+    def backtrace
+      if defined? Rails
+        Rails.backtrace_cleaner.clean(caller)
+      else
+        caller
+      end
+    end
+
+    def log_deprecated_attribute_usage(klass, *attrs)
+      warning_message = "WARNING: Called deprecated attribute for #{klass.name}: #{attrs.join(', ')}\n" +
+        backtrace.map { |trace| "\t#{trace}" }.join("\n")
+      if logger?
+        logger.warn do
+          "WARNING: Called deprecated attribute for #{klass.name}: #{attrs.join(', ')}\n" +
+            backtrace.map { |trace| "\t#{trace}" }.join("\n")
+        end
+      else
+        puts warning_message
       end
     end
   end

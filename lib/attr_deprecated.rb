172562f583514c168151ab461c07821e73accf7c
Anthony Erlinger <aerlinger@gmail.com>
2014-04-28 23:58:58 -0400

    Add logger capability to AttrDeprecated

--- lib/attr_deprecated.rb
+++ lib/attr_deprecated.rb
@@ -1,4 +1,5 @@
 require "attr_deprecated/version"
+require "notifiers/deprecation_logger"
 require "active_model/deprecated_attribute_set"
 
 require 'active_support/concern'
@@ -10,12 +11,14 @@ module AttrDeprecated
 
   included do
     class_attribute :_deprecated_attributes, instance_writer: false
-  end
 
+    @_deprecation_logger = AttrDeprecated::DeprecatedAttributeLogger.new(self)
+  end
 
   module ClassMethods
     ##
     # == attr_deprecated
+    #
     # class macro definition to non-destructively mark an attribute as deprecated.
     #
     # The original method (i.e. the one marked as deprecated) is renamed and wrapped in an alias that dispatches the notification.
@@ -23,42 +26,41 @@ module AttrDeprecated
     #
     def attr_deprecated(*attr_names)
       attr_names = DeprecatedAttributeSet.new(attr_names.compact)
-
       self._deprecated_attributes ||= DeprecatedAttributeSet.new
 
       # Taking the difference of the two sets ensures we don't deprecate the same attribute more than once
       (attr_names - _deprecated_attributes).each do |attribute|
-        set_attribute_as_deprecated attribute
+        _set_attribute_as_deprecated attribute
       end
 
       self._deprecated_attributes += attr_names
     end
 
     def deprecated_attributes
-      _deprecated_attributes.to_a
+      _deprecated_attributes
     end
 
     def clear_deprecated_attributes!
       self._deprecated_attributes = _deprecated_attributes.clear
     end
 
-    def set_attribute_as_deprecated(attribute)
+    def _set_attribute_as_deprecated(attribute)
       original_getter = "__deprecated_#{attribute}".to_sym
 
       if instance_methods.include?(attribute.to_sym)
         alias_method(original_getter.to_sym, attribute.to_sym)
+        klass = self
 
         define_method attribute.to_sym do |*args|
-          msg = <<-MESSAGE
-            WARNING: deprecated attribute #{original_getter} was called:
-            #{Thread.current.backtrace.join("\n")}
-          MESSAGE
-
-          puts msg
+          klass._notify_deprecated_attribute_call(klass, attribute)
 
           send(original_getter, *args)
         end
       end
+    end
+
+    def _notify_deprecated_attribute_call(klass, attribute)
+      @_deprecation_logger.log_deprecated_attribute_usage(klass, attribute)
     end
   end
 end
